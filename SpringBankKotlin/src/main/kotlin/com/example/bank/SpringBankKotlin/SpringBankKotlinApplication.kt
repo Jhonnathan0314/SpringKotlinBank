af4b45f9042ea1ee03a8dfd54d787675aa10ee3d
Jhonnathan0314 <jffc555@gmail.com>
2022-09-13 09:55:03 -0500

    Proyecto base funcionando
    Proyecto vacio pero ejecuta y conecta a la base de datos

---
+++ SpringBankKotlin/src/main/kotlin/com/example/bank/SpringBankKotlin/SpringBankKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bank.SpringBankKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBankKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBankKotlinApplication>(*args)
+}
